06376aa6e3f0c83e97dde90cea1ebccae29e6dca
“yoshiringo” <hey_kikokiko@hotmail.co.jp>
2023-04-04 21:55:54 +0900

    “データベースをpostgreSQLに変更”

--- project/settings.py
+++ project/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scoredb', # 作成したデータベース名
+        'USER': 'postgres', # 作成したユーザー名
+        'PASSWORD': '0kikokiko', # パスワード
+        'HOST': 'localhost', # localhostがデフォルト
+        'PORT': '5432' # 基本設定で5432
     }
 }
 
