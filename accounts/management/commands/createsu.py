e484cc9ff8c433ee451dcf8b238d75d8b1424fc8
“yoshiringo” <hey_kikokiko@hotmail.co.jp>
2023-05-25 18:16:44 +0900

    “eb用に更新”

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+import os
+import environ
 
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='yoshiringo').exists():
+        if not User.objects.filter(username=os.environ['CREATESU_NAME']).exists():
             User.objects.create_superuser(
-                username='yoshiringo',
-                password='0kikokiko'
+                username=os.environ['CREATESU_NAME'],
+                password=os.environ['CREATESU_PASS']
             )
         print('Superuser has been created.')
